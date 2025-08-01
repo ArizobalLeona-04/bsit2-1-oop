7ae8620977261730799709e2d5fb6f3887ab289d
Leona <lmarizobal34537@liceo.edu.ph>
2025-08-01 11:18:49 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[]args){
-        System.out.println("Main Branch!");
+import java.util.Scanner;
+
+public class LabActivity2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int total = 0;
+        int number;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            number = scanner.nextInt();
+            total += number;
+        }
+
+        System.out.println("\nTotal Number: " + total);
+        scanner.close();
     }
 }
