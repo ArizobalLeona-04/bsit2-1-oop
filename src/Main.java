9b66a5657a491f4a5ea68ca6fd9745933071fa42
Leona <lmarizobal34537@liceo.edu.ph>
2025-08-07 10:34:58 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,63 @@
-public class Main {
-    public static void main(String[]args){
-        System.out.println("Main Branch!");
+import java.util.Scanner;
+
+public class Student {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Student1");
+        String student1 = scanner.nextLine();
+
+        System.out.print("Name:");
+        String name = scanner.nextLine();
+
+        System.out.print("Age: ");
+        String age = scanner.nextLine();
+
+        System.out.print("Course: ");
+        String course = scanner.nextLine();
+
+        System.out.print("Grades: ");
+        String grades = scanner.nextLine();
+
+        System.out.print("Average: ");
+        String average = scanner.nextLine();
+
+        System.out.print("Letter Grade: ");
+        String lettergrade = scanner.nextLine();
+
+        System.out.print("Status: ");
+        String status = scanner.nextLine();
+
+
+        System.out.println("Student1"  );
+        System.out.println("Name: " + name );
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+        System.out.println("Grades: " + grades );
+        System.out.println("Average: " + average);
+        System.out.println("Letter Grade: " + lettergrade);
+
+
+        System.out.print("\nSummary : I give up na sir TvT ");
+        int Name = scanner.nextInt();
+
+        System.out.print("Age: ");
+        int Age = scanner.nextInt();
+
+
+        System.out.print("Course: ");
+        int Course = scanner.nextInt();
+
+
+
+
+        int total = Name + Age + Course ;
+        double Grades = (double) total / 400 * 100;
+        String remarks = Grades >= 75 ? "PASSED" : "FAILED";
+
+
+
+
+        scanner.close();
     }
 }
