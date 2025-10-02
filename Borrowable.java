2d6e4c05e0e4ac21a9626d57e44e23b76d4cff68
Leona <lmarizobal34537@liceo.edu.ph>
2025-10-02 10:50:25 +0800

    Update Borrowable.java

--- Borrowable.java
+++ Borrowable.java
@@ -1,1 +1,9 @@
+public interface Borrowable{
+    void borrowedItem();
+    void returnItem();
 
+    default String getBorrowingStatus() {
+        return "Available for Borrowing : Currently Borrowed";
+
+    }
+}
